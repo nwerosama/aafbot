ef2dc0a16e7632b61539f1a51d7e8f0b969ebb2c
nwerosama <96593068+nwerosama@users.noreply.github.com>
2026-03-16 07:31:57 +1100

    remove accidental filter for MessageCreate

--- src/events.rs
+++ src/events.rs
@@ -44,14 +44,8 @@ impl EventHandler for DiscordEvents {
     _ctx: &Context,
     mut event: Box<Event>
   ) -> Option<Box<Event>> {
-    // drop undocumented event
-    if event.name() == "GUILD_JOIN_REQUEST_UPDATE" {
-      return None
-    }
-
     // filter unimportant events out as i do not do anything with it to reduce memory footprint as much as possible.
     match &mut *event {
-      Event::MessageCreate(_) => return None,
       Event::MessageUpdate(_) => return None,
       Event::GuildCreate(evt) => {
         evt.guild.emojis = ExtractMap::new();
